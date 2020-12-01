9aa3db4bbd9418b5c6f12d8b9f49a050570fef17
MayuriDhale1807 <72254596+MayuriDhale1807@users.noreply.github.com>
2020-12-01 22:15:32 +0530

    Assignment1

---
+++ Q1.java
@@ -0,0 +1,18 @@
+package assignment_01;
+
+public class Q1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		System.out.print("Saurabh Kumar");
+	}
+}
+
+/*
+OUTPUT
+
+hello
+Saurabh Kumar
+
+*/
